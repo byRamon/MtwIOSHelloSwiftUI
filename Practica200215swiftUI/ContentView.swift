ee28a47c02353818868649409a25d51500fac6f9
ramon baeza yepez <By_ramon@outlook.com>
2020-02-29 11:10:04 -0600

    Done

--- Practica200215swiftUI/ContentView.swift
+++ Practica200215swiftUI/ContentView.swift
@@ -9,10 +9,62 @@
 import SwiftUI
 
 struct ContentView: View {
+    //@State private var tapCount = 0
+    //@State private var name = ""
+    let students = ["John","Doe","Alice"]
+    @State private var selectStudent = 0
     var body: some View {
-        Text("Hello, World!")
+        NavigationView{
+            Form{
+                Picker("Select your student", selection: $selectStudent){
+                    ForEach(0 ..< students.count){
+                        Text(self.students[$0])
+                    }
+                }
+                Text("You chose: Student #\(students[selectStudent])")
+            }
+        }
+        /*Form{
+            TextField("Enter your name", text:$name)
+            Text("Your name is \(name)")
+        }
+         VStack{
+             Button ("Tap count: \(self.tapCount)") {
+                 self.tapCount += 1
+                 print("click \(self.tapCount)")
+             }
+        }
+        Button(action:{
+            print("Click del boton")
+        }){
+            Text("Button")
+        }NavigationView{
+            Form{
+                Section{
+                    Text("Hello, World!")
+                        .font(.title)
+                    Text("Hello, World!")
+                    Text("Hello, World!")
+                    Text("Hello, World!")
+                    Text("Hello, World!")
+                    Text("Hello, World!")
+                    Text("Hello, World!")
+                    Text("Hello, World!")
+                    Text("Hello, World!")
+                    Text("Hello, World!")
+                }
+                Group{
+                    Text("Hello, World!")
+                        .font(.title)
+                    Text("Hello, World!")
+                    Text("Hello, World!")
+                    Text("Hello, World!")
+                    Text("Hello, World!")
+                }
+            }.navigationBarTitle("SwiftUI", displayMode: .inline)*/
+        }
     }
-}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
